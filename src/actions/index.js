5079caeb53fe0738a3fa2ece83db6500ed7fd78a
Misha Milovidov <m.alexeyvich@gmail.com>
2018-07-11 22:55:21 -0600

    Use post object in posts show component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
-export const FETCH_POSTS = 'FETCH_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=mmilovidov';
