fd83f0f15bae68cc243ddb715a151e0fa512ef58
Misha Milovidov <m.alexeyvich@gmail.com>
2018-07-11 22:36:43 -0600

    Add fetchpost action creator; add fetch post reducer

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
+
+class PostsShow extends Component {
+  render() {
+    return(
+      <div>Posts Show!</div>
+    )
+  }
+}
+
+export default connect(null, { fetchPost })(PostsShow);
